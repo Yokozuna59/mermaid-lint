ef714c0d044be88abd633d4afc493677fa8dca71
Yokozuna59 <u.yokozuna@gmail.com>
2023-05-21 17:55:31 +0300

    test(pie): update pie tests cases (#50)
    Signed-off-by: Yokozuna59 <u.yokozuna@gmail.com>

--- packages/mermaid-parser/tests/pie/syntax.test.ts
+++ packages/mermaid-parser/tests/pie/syntax.test.ts
@@ -9,7 +9,7 @@ import {
     PieChart,
 } from '../../src/language';
 
-describe('valid pie chart codes', () => {
+describe('when parsing pie chart', () => {
     let parser: (input: string) => Promise<LangiumDocument<Mermaid>>;
 
     beforeAll(async () => {
@@ -41,7 +41,7 @@ describe('valid pie chart codes', () => {
         \tpie
 
         `,
-    ])('should handle valid pie definitions', async (str: string) => {
+    ])('should handle valid pie', async (str: string) => {
         const result = (await parser(str)).parseResult;
         expect(result.parserErrors).toHaveLength(0);
         expect(result.lexerErrors).toHaveLength(0);
@@ -70,17 +70,137 @@ describe('valid pie chart codes', () => {
         pie\tshowData
 
         `,
+    ])('should handle valid pie + showData', async (str: string) => {
+        const result = (await parser(str)).parseResult;
+        expect(result.parserErrors).toHaveLength(0);
+        expect(result.lexerErrors).toHaveLength(0);
+
+        const value = result.value;
+        expect(value.$type).toBe(PieChart);
+        expect(value.showData).toBeTruthy();
+        expect(value.title).toBeUndefined();
+        expect(value.sections).toHaveLength(0);
+    });
+
+    // pie + title
+    it.each([
+        // without whitespaces
+        `pie title sample title`,
+
+        // with spaces
+        `  pie  title sample title  `,
+
+        // with tabs
+        `\tpie\ttitle sample title\t`,
+
+        // with extra whitespaces
+        `pie
+
+        \ttitle sample title
+
+        `,
+    ])('should handle valid pie + title in same line', async (str: string) => {
+        const result = (await parser(str)).parseResult;
+        // expect(result.parserErrors).toHaveLength(0);
+        // expect(result.lexerErrors).toHaveLength(0);
+
+        const value = result.value;
+        expect(value.$type).toBe(PieChart);
+        expect(value.showData).toBeFalsy();
+        expect(value.title).toBe('sample title');
+        expect(value.sections).toHaveLength(0);
+    });
+
+    // pie + \n + title
+    it.each([
+        // without newlines
+        `pie
+        title sample title`,
+
+        // extra newline after
+        `pie
+        title sample title
+        `,
+
+        // extra newline before
+        `pie
+
+        title sample title`,
+
+        // extra newlines
+        `pie
+
+        title sample title
+
+        `,
     ])(
-        'should handle valid pie + showData definitions',
+        'should handle valid pie + title in different line',
         async (str: string) => {
             const result = (await parser(str)).parseResult;
-            expect(result.parserErrors).toHaveLength(0);
-            expect(result.lexerErrors).toHaveLength(0);
+            // expect(result.parserErrors).toHaveLength(0);
+            // expect(result.lexerErrors).toHaveLength(0);
+
+            const value = result.value;
+            expect(value.$type).toBe(PieChart);
+            expect(value.showData).toBeFalsy();
+            expect(value.title).toBe('sample title');
+            expect(value.sections).toHaveLength(0);
+        },
+    );
+
+    // pie + showData + title
+    it.each([
+        // without newlines
+        `pie showData title sample title`,
+
+        // extra newline after
+        `pie showData title sample title
+        `,
+    ])('should handle valid pie + showData + title', async (str: string) => {
+        const result = (await parser(str)).parseResult;
+        // expect(result.parserErrors).toHaveLength(0);
+        // expect(result.lexerErrors).toHaveLength(0);
+
+        const value = result.value;
+        expect(value.$type).toBe(PieChart);
+        expect(value.showData).toBeTruthy();
+        expect(value.title).toBe('sample title');
+        expect(value.sections).toHaveLength(0);
+    });
+
+    // pie + showData + \n + title
+    it.each([
+        // without newlines
+        `pie showData
+        title sample title`,
+
+        // extra newline after
+        `pie showData
+        title sample title
+        `,
+
+        // extra newline before
+        `pie showData
+
+        title sample title`,
+
+        // extra newlines
+        `pie showData
+
+        title sample title
+
+        `,
+    ])(
+        'should handle valid pie + showData + title in different line',
+        async (str: string) => {
+            const result = (await parser(str)).parseResult;
+            // expect(result.parserErrors).toHaveLength(0);
+            // expect(result.lexerErrors).toHaveLength(0);
 
             const value = result.value;
             expect(value.$type).toBe(PieChart);
             expect(value.showData).toBeTruthy();
-            expect(value.title).toBeUndefined();
+            expect(value.title).toBe('sample title');
             expect(value.sections).toHaveLength(0);
         },
     );
