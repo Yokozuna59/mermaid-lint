773ba76eb0138a13164b5d3ef56ae731e350733e
Yokozuna59 <u.yokozuna@gmail.com>
2023-05-25 21:03:25 +0300

    feat: add multiple title declaration
    Signed-off-by: Yokozuna59 <u.yokozuna@gmail.com>

--- packages/mermaid-parser/src/language/matchers/title.ts
+++ packages/mermaid-parser/src/language/matchers/title.ts
@@ -1,15 +1,21 @@
 /* eslint-disable unicorn/no-null */
-import { CustomPatternMatcherFunc } from 'chevrotain';
+import {
+    CustomPatternMatcherFunc,
+    CustomPatternMatcherReturn,
+} from 'chevrotain';
 
 /**
  * Matches a single title
  */
-const titleRegex = /(?:^|[\t ]+)title(?:[\t ]+([^\n]*)|$)/;
+const titleRegex = /(?:^|[\t ]+)title(?:[\t ]+([^\n]+)?|$)/;
 export const matchTitle: CustomPatternMatcherFunc = (string_: string) => {
-    const match = titleRegex.exec(string_);
-    if (match && match[1] !== undefined) {
-        return [match[1].trim()];
+    let result: CustomPatternMatcherReturn | null = null;
+    let match = titleRegex.exec(string_);
+    while (match !== null) {
+        result = match[1] === undefined ? null : [match[1].trim()];
+        string_ = string_.replace(match[0], '');
+        match = titleRegex.exec(string_);
     }
-    return null;
+    return result;
 };
 /* eslint-enable unicorn/no-null */
