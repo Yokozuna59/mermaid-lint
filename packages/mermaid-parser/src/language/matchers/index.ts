f26e74140c8e6028170fb478476ee8d093d6a4e3
Yokozuna59 <u.yokozuna@gmail.com>
2023-05-31 00:53:21 +0300

    fix: use offest in matchers instead of loop
    Signed-off-by: Yokozuna59 <u.yokozuna@gmail.com>

--- packages/mermaid-parser/src/language/matchers/index.ts
+++ packages/mermaid-parser/src/language/matchers/index.ts
@@ -1,3 +1,7 @@
 export * from './accdescr';
 export * from './acctitle';
 export * from './title';
+
+export type CustomMatcherReturn =
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (RegExpExecArray & { payload?: any }) | null;
